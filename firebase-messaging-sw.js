98e84ddc18ebc5c523acd54e5be1c0268b905df2
Азирет Асангазиев <a.asangaziev@bftcom.com>
2024-06-13 11:20:27 +0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -30,3 +30,13 @@ messaging.setBackgroundMessageHandler(function(payload) {
 
   return self.registration.showNotification(payload.data.title, payload.data);
 });
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../firebase-messaging-sw.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    }).catch(function(err) {
+      console.log('Service worker registration failed, error:', err);
+    });
+  }
